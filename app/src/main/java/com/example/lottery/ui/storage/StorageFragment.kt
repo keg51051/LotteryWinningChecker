3242953ac4e732b8af3fcc3c7c9a329e36a52ad7
Hanseong Kim <keg51051@gmail.com>
2019-12-09 18:51:56 -0500

    storage

---
+++ app/src/main/java/com/example/lottery/ui/storage/StorageFragment.kt
@@ -0,0 +1,32 @@
+package com.example.lottery.ui.storage
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lottery.R
+
+class StorageFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = StorageFragment()
+    }
+
+    private lateinit var viewModel: StorageViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.storage_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(StorageViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/lottery/ui/storage/StorageViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.lottery.ui.storage
+
+import androidx.lifecycle.ViewModel
+
+class StorageViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
